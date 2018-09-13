ec6d249dfc03eee43b9327250a8c7b38413c731b
Enrique <engapa@gmail.com>
2018-09-13 15:14:08 +0200

    Add a sh file to save your custom environment

---
+++ k8s-gke.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Update with your own values
+
+GCP_CREDENTIALS=/path/project-*******.json
+GCP_ZONE=europe-west4-a
+GCP_PROJECT_ID=my_project
+GKE_CLUSTER_NAME=my_cluster
+GITHUB_TOKEN=**********
+GKE_CLUSTER_VERSION=1.10.7-gke.1
+GKE_NODES=2
+GKE_NODES_MAX=5
+GKE_IMAGE_TYPE=n1-standard-8
